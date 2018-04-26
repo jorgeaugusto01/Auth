6d3ef0780374a86db70ce0740e3c457fc7b7157c
jorgeaugusto01 <jorge.araujo@gmail.com>
2018-04-26 14:36:56 -0300

    26/04/2018

---
+++ __init__.py
@@ -0,0 +1,68 @@
+# project/__init__.py
+
+from flask import Flask
+from flask.ext.bcrypt import bcrypt
+from flask.ext.sqlalchemy import SQLAlchemy
+from config import BaseConfig
+from flask import request, jsonify, session
+from models import User
+
+
+# config
+
+app = Flask(__name__)
+app.config.from_object(BaseConfig)
+
+bcrypt = Bcrypt(app)
+db = SQLAlchemy(app)
+
+
+# routes
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    json_data = request.json
+    user = User(
+        email=json_data['email'],
+        password=json_data['password']
+    )
+    try:
+        db.session.add(user)
+        db.session.commit()
+        status = 'success'
+    except:
+        status = 'this user is already registered'
+    db.session.close()
+    return jsonify({'result': status})
+
+
+@app.route('/api/logout')
+def logout():
+    session.pop('logged_in', None)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    json_data = request.json
+    user = User.query.filter_by(email=json_data['email']).first()
+    if user and bcrypt.check_password_hash(
+            user.password, json_data['password']):
+        session['logged_in'] = True
+        status = True
+    else:
+        status = False
+    return jsonify({'result': status})
+
+@app.route('/api/status')
+def status():
+    if session.get('logged_in'):
+        if session['logged_in']:
+            return jsonify({'status': True})
+    else:
+        return jsonify({'status': False})
